0cf965059a744e20f6c66ebf1fdef3636f5506b9
Carlos Hernandez <khemtz@gmail.com>
2022-03-15 12:52:48 -0600

    nada que hacer

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
   @override
+
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
   Widget build(BuildContext context) {
     // TODO: implement build
     final fullstar = Container (
@@ -26,37 +29,44 @@ class DescriptionPlace extends StatelessWidget {
       ),
     );
 
-    final title_stars = Row(
-    children: <Widget>[
-    Container (
-      margin: EdgeInsets.only(
-        top: 320.0,
-        left: 20.0,
-        right: 20.0
-      ),
-      child: Text(
-        "Duwili Ella",
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.w900,
+    final namePlace = Container (
+        margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0
         ),
-        textAlign: TextAlign.center,
-      ),
-
-    ),
-      Row(
-        children: [
-          fullstar,
-          fullstar,
-          fullstar,
-          fullstar,
-          halfstar
-        ],
-      )
-    ],
+        child: Text(
+          "Duwili Ella",
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.w900,
+          ),
+          textAlign: TextAlign.center,
+        )
     );
 
+    final estrellitas =  Row(
+      children: [
+        fullstar,
+        fullstar,
+        fullstar,
+        fullstar,
+        fullstar
+      ],
+    );
+
+    final title_stars = Row(
+        children: <Widget>[
+          namePlace,
+          estrellitas,
+        ],
+        );
+
+
+
+
     return title_stars;
+
   }
 
 }
